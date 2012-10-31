cafece11b227606dd8b714c16c72879ae3ac5a55
Aaron Muylaert <aaron@tactics.be>
2012-10-31 07:49:49 +0100

    Apply code standards and fix indentation.

--- Controller/TacticsController.php
+++ Controller/TacticsController.php
@@ -4,21 +4,20 @@ namespace Tactics\Bundle\AdminBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-
 class TacticsController extends Controller
 {
     /**
      *  Creates an not found exception if the objects does not exists
-     * 
+     *
      * @param Object
      * @param (optional) String $type of the object
      */
     public function createExceptionIfNotFound($object, $type = false)
-    {     
-      if(! $object) {
-       $notice = ($type ? $type : 'Object') . ' not found.';
-       throw $this->createNotFoundException($notice);
-      }
+    {
+        if (! $object) {
+            $notice = ($type ? $type : 'Object') . ' not found.';
+            throw $this->createNotFoundException($notice);
+        }
     }
-    
+
 }
