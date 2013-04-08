22a08d2597a7c5c6a324ebd0c86a4d408ee10b69
Aaron Muylaert <aaron@tactics.be>
2013-04-08 22:24:00 +0200

    First step in right direction for a functional testing toolset.

--- Tests/Controller/TacticsWebTestCase.php
+++ Tests/Controller/TacticsWebTestCase.php
@@ -5,10 +5,69 @@ namespace Tactics\Bundle\AdminBundle\Tests\Controller;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Component\BrowserKit\Cookie;
 use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
+use Doctrine\Common\Persistence\ObjectManager;
 
 class TacticsWebTestCase extends WebTestCase
 {
-    
+    public function setUp()
+    {
+        static::$kernel = static::createKernel(array(
+            'environment' => 'test',
+            'debug' => true,
+        ));
+
+        static::$kernel->boot();
+    }
+
+    public function loadFixtures(ObjectManager $om, $dir = null)
+    {
+        $loader    = new \Nelmio\Alice\Loader\Yaml();
+        $persister = new \Nelmio\Alice\ORM\Doctrine($om);
+
+        $this->regenerateSchema($om);
+
+        $persister->persist($loader->load($dir));
+    }
+
+    public function regenerateSchema(ObjectManager $om)
+    {
+        $metadatas = $om->getMetadataFactory()->getAllMetadata();
+
+        if (! empty($metadatas)) {
+            $tool = new \Doctrine\ORM\Tools\SchemaTool($om);
+            $tool->dropSchema($metadatas);
+            $tool->createSchema($metadatas);
+        }
+    }
+
+    public function getFosUserClient($usernameOrEmail, $password)
+    {
+        $manager = $this->get('fos_user.user_manager');
+        $em      = $this->get('doctrine')->getEntityManager();
+
+        $user = $manager->findUserByUsernameOrEmail($usernameOrEmail);
+
+        // TODO FOSUser __construct sets a salt. Can I do anything about this?
+        $user->setPassword($user->getPassword() . '{' . $user->getSalt() . '}');
+
+        $manager->updateUser($user);
+        $manager->reloadUser($user);
+
+        $client = static::createClient(array(), array(
+            'PHP_AUTH_USER' => $usernameOrEmail,
+            'PHP_AUTH_PW'   => $password,
+        ));
+
+        $client->followRedirects(true);
+
+        return $client;
+    }
+
+    public function get($serviceId)
+    {
+        return static::$kernel->getContainer()->get($serviceId);
+    }
+
     /**
      * Returns an authentificated client
      * 
