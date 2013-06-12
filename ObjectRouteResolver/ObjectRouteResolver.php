4912f50805dab073f763c53985564b40ccd01a04
Bart Ruysseveldt <bart@tactics.be>
2013-06-12 14:53:20 +0200

    Remove container from the object route resolver

--- ObjectRouteResolver/ObjectRouteResolver.php
+++ ObjectRouteResolver/ObjectRouteResolver.php
@@ -10,32 +10,17 @@ use Tactics\Bundle\AdminBundle\ObjectRouteResolver\Exception\UnknownClassExcepti
 /**
  * @author Aaron Muylaert <aaron.muylaert at tactics.be>
  */
-class ObjectRouteResolver implements ContainerAwareInterface
+class ObjectRouteResolver
 {
       /**
-       * @var ContainerInterface $container A ContainerInterface instance.
-       */
-      protected $container;
-
-      /**
-       * @var array $defaultObjectRoutes An array containing the default object 
+       * @var array $defaultObjectRoutes An array containing the default object
        * routes.
        */
       protected $defaultObjectRoutes = array();
-      
-      public function __construct(ContainerInterface $container)
-      {
-          $this->setContainer($container);
 
-          $this->defaultObjectRoutes = $this->container->getParameter('object_routes');
-      }
-
-      /**
-       * {@inheritdoc}
-       */
-      public function setContainer(ContainerInterface $container = null)
+      public function __construct(array $routes)
       {
-          $this->container = $container;
+          $this->defaultObjectRoutes = $routes;
       }
 
       /**
@@ -47,7 +32,7 @@ class ObjectRouteResolver implements ContainerAwareInterface
       public function retrieveByClass($class)
       {
           if (false === array_key_exists($class, $this->defaultObjectRoutes)) {
-              throw new UnknownClassException($class);       
+              throw new UnknownClassException($class);
           }
 
           return $this->defaultObjectRoutes[$class];
