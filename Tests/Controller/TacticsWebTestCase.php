e445a1fdb6806cd7ec846ed36c7c89d72624798d
Bart Ruysseveldt <bart@tactics.be>
2013-06-12 17:32:45 +0200

    Make tests faster

--- Tests/Controller/TacticsWebTestCase.php
+++ Tests/Controller/TacticsWebTestCase.php
@@ -4,59 +4,138 @@ namespace Tactics\Bundle\AdminBundle\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Component\BrowserKit\Cookie;
-use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
-use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\ORM\Tools\SchemaTool;
+use Doctrine\DBAL\Driver\PDOSqlite\Driver as SqliteDriver;
+use Doctrine\Common\Persistence\ManagerRegistry;
 
 class TacticsWebTestCase extends WebTestCase
 {
-    public function setUp()
-    {
-        static::$kernel = static::createKernel(array(
-            'environment' => 'test',
-            'debug' => true,
-        ));
+    /**
+     * @var array
+     */
+    static private $cachedMetadatas = array();
 
-        static::$kernel->boot();
+    /**
+     * @var array
+     */
+    static private $cachedMetadatasSerialized = array();
+
+    /**
+     * Determine if the Fixtures that define a database backup have been
+     * modified since the backup was made.
+     *
+     * @param array $classNames The fixture classnames to check
+     * @param string $backup The fixture backup SQLite database file path
+     *
+     * @return bool TRUE if the backup was made since the modifications to the
+     * fixtures; FALSE otherwise
+     */
+    protected function isBackupUpToDate(array $files, $backup)
+    {
+        $backupLastModifiedDateTime = new \DateTime();
+        $backupLastModifiedDateTime->setTimestamp(filemtime($backup));
+
+        foreach ($files as $file) {
+            $fixtureLastModifiedDateTime = new \DateTime('@'.filemtime($file));
+            if ($backupLastModifiedDateTime < $fixtureLastModifiedDateTime) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
-    public function loadFixtures($dir, ObjectManager $om = null)
+    public function loadFixtures($files, $omName = null, $registryName = 'doctrine', $cache = true)
     {
-        if (null === $om) {
-            $om = $this->get('doctrine')->getManager();
+        $files = (array) $files;
+        $container = static::$kernel->getContainer();
+
+        $registry = $container->get($registryName);
+        if ($registry instanceof ManagerRegistry) {
+            $om = $registry->getManager($omName);
+            $type = $registry->getName();
+        } else {
+            $om = $registry->getEntityManager($omName);
+            $type = 'ORM';
+        }
+
+        $cacheDriver = $om->getMetadataFactory()->getCacheDriver();
+
+        if ($cacheDriver) {
+            $cacheDriver->deleteAll();
+        }
+
+        if ('ORM' === $type) {
+            $connection = $om->getConnection();
+            if ($connection->getDriver() instanceOf SqliteDriver) {
+                $params = $connection->getParams();
+                $name = isset($params['path']) ? $params['path'] : $params['dbname'];
+
+                if (!isset(self::$cachedMetadatas[$omName])) {
+                    self::$cachedMetadatas[$omName] = $om->getMetadataFactory()->getAllMetadata();
+                    self::$cachedMetadatasSerialized[$omName] = self::$cachedMetadatas[$omName];
+                }
+                $metadatas = self::$cachedMetadatas[$omName];
+
+                if ($cache) {
+                    $backup = $container->getParameter('kernel.cache_dir') . '/test_' . md5(self::$cachedMetadatasSerialized[$omName] . serialize($files)) . '.db';
+                    if (file_exists($backup) && $this->isBackupUpToDate($files, $backup)) {
+                        $om->flush();
+                        $om->clear();
+
+                        copy($backup, $name);
+
+                        $this->postFixtureRestore();
+
+                        return;
+                    }
+                }
+
+                $schemaTool = new SchemaTool($om);
+                $schemaTool->dropDatabase($name);
+                if (!empty($metadatas)) {
+                    $schemaTool->createSchema($metadatas);
+                }
+                $this->postFixtureSetup();
+            }
         }
 
         $loader    = new \Nelmio\Alice\Loader\Yaml();
         $persister = new \Nelmio\Alice\ORM\Doctrine($om);
 
-        $this->regenerateSchema($om);
+        foreach ($files as $fixture) {
+            $persister->persist($loader->load($fixture));
+        }
+//        $loader = $this->getFixtureLoader($container, $classNames);
 
-        $persister->persist($loader->load($dir));
+        if (isset($name) && isset($backup)) {
+            copy($name, $backup);
+        }
     }
 
-    public function regenerateSchema(ObjectManager $om)
+    /**
+     * Callback function to be executed after Schema creation.
+     * Use this to execute acl:init or other things necessary.
+     */
+    protected function postFixtureSetup()
     {
-        $metadatas = $om->getMetadataFactory()->getAllMetadata();
 
-        if (! empty($metadatas)) {
-            $tool = new \Doctrine\ORM\Tools\SchemaTool($om);
-            $tool->dropSchema($metadatas);
-            $tool->createSchema($metadatas);
-        }
+    }
+
+    /**
+     * Callback function to be executed after Schema restore.
+     */
+    protected function postFixtureRestore()
+    {
+
     }
 
     public function getFosUserClient($usernameOrEmail, $password)
     {
-        $manager = $this->get('fos_user.user_manager');
-        $em      = $this->get('doctrine')->getEntityManager();
-
-        $user = $manager->findUserByUsernameOrEmail($usernameOrEmail);
-
-        // TODO FOSUser __construct sets a salt. Can I do anything about this?
-        $user->setPassword($user->getPassword() . '{' . $user->getSalt() . '}');
-
-        $manager->updateUser($user);
-
-        $client = static::createClient(array(), array(
+        $client = static::createClient(array(
+            'environment' => 'test',
+            'debug' => false,
+        ), array(
             'PHP_AUTH_USER' => $usernameOrEmail,
             'PHP_AUTH_PW'   => $password,
         ));
@@ -86,16 +165,16 @@ class TacticsWebTestCase extends WebTestCase
 
     // POSSIBILITY 2
     /*public function createAuthentifcatedClient($firewall, $role = 'ROLE_USER')
-    {   
-        $client = static::createClient(); 
-        $container = static::$kernel->getContainer();        
-        $client->getCookieJar()->set(new Cookie($container->get('session')->getName(), true));        
-        $token = new UsernamePasswordToken('admin', null, $firewall, $role);        
-        $container->get('security.context')->setToken($token);        
+    {
+        $client = static::createClient();
+        $container = static::$kernel->getContainer();
+        $client->getCookieJar()->set(new Cookie($container->get('session')->getName(), true));
+        $token = new UsernamePasswordToken('admin', null, $firewall, $role);
+        $container->get('security.context')->setToken($token);
         $container->get('session')->set('_security_' . $firewall, serialize($token));
-        
+
         return $client;
-        
+
     }*/
 
     /**
