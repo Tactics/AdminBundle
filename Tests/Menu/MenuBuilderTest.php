a251745d5155ed03549c0186e4771b90c335a60b
Aaron Muylaert <aaron@tactics.be>
2014-01-30 12:39:33 +0100

    Test action user access when action has multiple roles defined.

--- Tests/Menu/MenuBuilderTest.php
+++ Tests/Menu/MenuBuilderTest.php
@@ -13,6 +13,8 @@ class MenuBuilderTest extends \PHPUnit_Framework_TestCase
         'SEARCH_FISH' => false,
         'SEARCH_CATS' => false,
         'SEARCH_DEER' => false,
+        'DELETE_FISH' => false,
+        'FISH_MASTER' => false,
     );
 
     public function setUp()
@@ -31,7 +33,9 @@ class MenuBuilderTest extends \PHPUnit_Framework_TestCase
                 ),
                 'Fish' => array(
                     'actions' => array(
-                        array('label' => 'Search', 'role' => 'SEARCH_FISH') // action
+                        array('label' => 'Search fish', 'role' => 'SEARCH_FISH'), // action
+                        array('label' => 'Create fish'),
+                        array('label' => 'Delete fish', 'role' => array('DELETE_FISH', 'FISH_MASTER'))
                     ),
                 ),
                 'Deer' => array(
@@ -82,6 +86,35 @@ class MenuBuilderTest extends \PHPUnit_Framework_TestCase
 
     /**
      * @test
+     * @dataProvider setupMaps
+     */
+    public function it_removes_action_when_user_does_not_have_one_of_roles($roleMap, $expected)
+    {
+        $menu = $this->buildMenu($roleMap);
+
+        $this->assertEquals($expected, isset($menu['Animals']['Fish']['actions'][2]));
+    }
+
+    public function setupMaps()
+    {
+        // Using array_merge to clone arrays.
+        $userHasRoleFishMaster = array_merge(array(), $this->roleMap);
+        $userHasRoleFishMaster['FISH_MASTER'] = true;
+
+        $userHasRoleDeleteFish = array_merge(array(), $this->roleMap);
+        $userHasRoleDeleteFish['DELETE_FISH'] = true;
+
+        $userDoesNotHaveRoles = array_merge(array(), $this->roleMap);
+
+        return array(
+            array($userHasRoleFishMaster, true),
+            array($userHasRoleDeleteFish, true),
+            array($userDoesNotHaveRoles, false),
+        );
+    }
+
+    /**
+     * @test
      */
     public function it_removes_subitem_when_it_does_not_contain_actions()
     {
@@ -102,10 +135,14 @@ class MenuBuilderTest extends \PHPUnit_Framework_TestCase
         $this->roleMap[$role] = false;
     }
 
-    private function buildMenu()
+    /**
+     * Only pass roles array for advanced configuration that can't be achieved by
+     * using the helper methods that directly manipulate the roleMap instance variable.
+     */
+    private function buildMenu(array $roles = array())
     {
         // For using in Closure (PHP 5.3 >.<)
-        $roleMap = $this->roleMap;
+        $roleMap = $roles ? $roles : $this->roleMap;
 
         // Set up configured user roles
         $this->security->expects($this->any())
