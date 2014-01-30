169305a1e9cb56c230b49a146799cfbbd7f41183
Aaron Muylaert <aaron@tactics.be>
2014-01-30 11:27:41 +0100

    Test menu item is unset when it contains no children.

--- Tests/Menu/MenuBuilderTest.php
+++ Tests/Menu/MenuBuilderTest.php
@@ -2,6 +2,37 @@
 
 namespace Tactics\Bundle\AdminBundle\Tests\Menu;
 
+use Tactics\Bundle\AdminBundle\Menu\MenuBuilder;
+
 class MenuBuilderTest extends \PHPUnit_Framework_TestCase
 {
+    private $menu;
+    private $security;
+    private $builder;
+
+    public function setUp()
+    {
+        $this->security = $this->getMockBuilder('Symfony\Component\Security\Core\SecurityContextInterface')
+            ->disableOriginalConstructor()
+            ->setMethods(array('isGranted', 'getToken', 'setToken'))
+            ->getMock();
+
+        $this->menu = array(
+            'Airplanes' => array(),
+            'Animals' => array('Dogs' => array()),
+        );
+
+        $this->builder = new MenuBuilder($this->security);
+    }
+
+    /**
+     * @test
+     */
+    public function it_removes_item_when_it_has_no_children()
+    {
+        $menu = $this->builder->build($this->menu);
+
+        $this->assertFalse(isset($menu['Airplanes']));
+        $this->assertTrue(isset($menu['Animals']));
+    }
 }
